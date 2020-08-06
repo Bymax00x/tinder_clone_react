33d61f8b811d59012ac7f3d4d38f50ba8433bcf7
Bymax00x <bymaxmndr@gmail.com>
2020-08-06 08:31:24 +0545

    updated tutorial before react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     
+      <Header />
+      
+      <h1>Tinder clone</h1>
+      {/* header
+      tinder card
+      card with buttons
+
+
+      chat system
+      individual chat system */}
     </div>
   );
 }
